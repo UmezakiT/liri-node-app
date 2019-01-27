d11be45cee9166e6e62cee4b5abe6ee7ae533b1f
Tatsuya <islanders2490@gmail.com>
2019-01-27 02:38:55 -0500

    adding files and folders with unfinished functions

---
+++ liri.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const axios = require("axios");
+const moment = require("moment");
+let keys = require("./keys.js");
+let spotifyNode = require("node-spotify-api");
+let spotify = new spotifyNode(keys.spotify);
+var arg = process.argv[2];
+var arg2 = process.argv[3];
+
+
+function spotifySearch() {
+
+  spotify
+    .search({ type: 'track', query: arg2})
+    .then(function (res) {
+      const type = res.tracks.items[0]
+      console.log(`Artist: ${type.album.artists[0].name} || Song: ${song.name} || Album: ${type.album.name} || Link: ${type.preview_url}`);
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+
+function movieSearch() {
+
+  axios.get("http://www.omdbapi.com/?t=" + arg2 + "&y=&plot=short&apikey=trilogy").then(
+    function (res) {
+      console.log(`Movie: ${res.data.Title} || Rating: ${res.data.imdbRating} || Release Date: ${res.data.Released} || Actors: ${res.data.Actors}`);
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+function bandSearch(){
+  axios.get("https://rest.bandsintown.com/artists/" + userCommand2 + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      console.log(`Date: ${moment(date).format("MM/DD/YYYY")} || Venue: ${response.data[0].venue.name} || Country: ${response.data[0].venue.country} || City: ${response.data[0].venue.city}`);
+    })
+}
+
+function search(){
+
+}
+
+
+switch (arg) {
+  case "spotify-this-song":
+    spotifySearch();
+    break;
+  case "movie-this":
+    movieSearch();
+    break;
+  case "band":
+    bandSearch(); 
+    break;
+  // case "do-what-it-says":
+  //   search();
+  //   break;
+}
